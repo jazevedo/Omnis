4e693b9b0d1fb6b75142a523f493d6a083298e9c
Antonio Haley <antonio.haley@gmail.com>
2020-04-05 11:01:03 -0400

    Initial commit

---
+++ app/src/main/java/com/example/respirator/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.respirator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+
+}
